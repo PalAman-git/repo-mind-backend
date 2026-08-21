aef68dbbc11b6f9fd9b993863972bec913c400dd
Aman Pal <palaman2626@gmail.com>
2026-08-21 08:28:33 +0530

    refactor: github service

--- src/github/github.service.ts
+++ src/github/github.service.ts
@@ -59,33 +59,47 @@ export class GithubService {
         return response.json();
     }
 
+    async getRepoLanguages(owner:string,repo:string){
+        const response = await fetch(`https://api.github.com/repos/${owner}/${repo}/languages`,{
+            headers:{
+                Accept:'application/vnd.github+json',
+            }
+        })
+
+        if(!response.ok){
+            throw new Error(`Github API error: ${response.status}`);
+        }
+
+        return response.json();
+    }
+
     async getFileContent(owner: string,repo: string,path: string,) 
-        {
-            const response = await fetch(
-                `https://api.github.com/repos/${owner}/${repo}/contents/${encodeURIComponent(path)}`,
-                {
+    {
+        const response = await fetch(
+            `https://api.github.com/repos/${owner}/${repo}/contents/${encodeURIComponent(path)}`,
+            {
                 headers: {
                     Accept: 'application/vnd.github+json',
                 },
-                },
-            );
+            },
+        );
 
-            if (!response.ok) {
-                throw new Error(`GitHub API error: ${response.status}`);
-            }
-
-            const data = await response.json();
-
-            if (Array.isArray(data)) {
-                throw new Error(`${path} is a directory`);
-            }
-
-            if (data.encoding !== 'base64') {
-                throw new Error(`Unexpected encoding for ${path}`);
-            }
-
-            return Buffer.from(data.content, 'base64').toString('utf-8');
+        if (!response.ok) {
+            throw new Error(`GitHub API error: ${response.status}`);
         }
+
+        const data = await response.json();
+
+        if (Array.isArray(data)) {
+            throw new Error(`${path} is a directory`);
+        }
+
+        if (data.encoding !== 'base64') {
+            throw new Error(`Unexpected encoding for ${path}`);
+        }
+
+        return Buffer.from(data.content, 'base64').toString('utf-8');
+    }
 
 
 }
